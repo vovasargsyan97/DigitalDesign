eddfeced26fd4465852ee552342536e346f14536
Владимир <60507008+vovasargsyan97@users.noreply.github.com>
2021-10-25 15:45:59 +0300

    Add files via upload

---
+++ Homework 3 - Final Project C#/WebApplication3/CountOccurence/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CountOccurence
+{
+    public class Class1
+    {
+        public Task<Dictionary<string, int>> CountOccurence(string text)
+        {
+            string textToLower = text.ToLower();
+
+            Regex reg_exp = new Regex("[^а-я0-9]");
+            textToLower = reg_exp.Replace(textToLower, " ");
+            string[] Value = textToLower.Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> CountTheOccurrences = new Dictionary<string, int>();
+            for (int i = 0; i < Value.Length; i++)                                       // Loop the splited string  
+            {
+                if (CountTheOccurrences.ContainsKey(Value[i]))                           // Check if word is already in dictionary update the count  
+                {
+                    int value = CountTheOccurrences[Value[i]];
+                    CountTheOccurrences[Value[i]] = value + 1;
+                }
+                else
+                // If we found the same word we just increase the count in the dictionary 
+                {
+                    CountTheOccurrences.Add(Value[i], 1);
+                }
+            }
+            CountTheOccurrences = CountTheOccurrences.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            return Task.FromResult(CountTheOccurrences);
+        }
+    }
+}
---
+++ Homework 3 - Final Project C#/WebApplication3/WebApplication3/Controllers/ExampleController.cs
@@ -0,0 +1,50 @@
+﻿using CountOccurence;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<Dictionary<string, int>> GetUsedWordsInText(string filePath)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            string text = System.IO.File.ReadAllText(filePath);
+            Dictionary<string, int> CountWords = new Dictionary<string, int>();
+
+            Class1 obj = new();
+            CountWords = await obj.CountOccurence(text);
+
+            //var method = obj.GetType().GetMethod("CountOccurence", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            //CountWords = (Dictionary<string, int>)method.Invoke(obj, new object[] { text });
+
+            // StringBuilder sb = new StringBuilder();
+
+            foreach (var kvp in CountWords)
+            {
+                StreamWriter sw = new StreamWriter(@"C:\gago\Occurence.txt", true, Encoding.UTF8);
+
+
+                sw.Write(kvp.Key + "--" + kvp.Value + System.Environment.NewLine);
+
+                //close the file
+                sw.Close();
+            }
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+            return CountWords;
+        }
+    }
+}
